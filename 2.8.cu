fe9eb8ef7ca7d94b67fb3e6fc85cecd33ca79a75
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2026-05-02 13:06:54 +0500

    2.8

---
+++ 2.8.cu
@@ -0,0 +1,92 @@
+#include <cuda_runtime.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+__global__ void vecAddKernel(float *A, float *B, float *C, int n) {
+    int i = threadIdx.x + blockDim.x * blockIdx.x;
+    if (i < n) {
+        C[i] = A[i] + B[i];
+    }
+}
+
+void vecAdd(float *A_h, float *B_h, float *C_h, int n) {
+    int size = n * sizeof(float);
+    float *A_d, *B_d, *C_d;
+
+    cudaError_t err = cudaMalloc((void **)&A_d, size);
+    if (err != cudaSuccess) {
+        printf("%s in %s at line %d\n", cudaGetErrorString(err), __FILE__, __LINE__);
+        exit(EXIT_FAILURE);
+    }
+
+    err = cudaMalloc((void **)&B_d, size);
+    if (err != cudaSuccess) {
+        printf("%s in %s at line %d\n", cudaGetErrorString(err), __FILE__, __LINE__);
+        exit(EXIT_FAILURE);
+    }
+
+    err = cudaMalloc((void **)&C_d, size);
+    if (err != cudaSuccess) {
+        printf("%s in %s at line %d\n", cudaGetErrorString(err), __FILE__, __LINE__);
+        exit(EXIT_FAILURE);
+    }
+
+    err = cudaMemcpy(A_d, A_h, size, cudaMemcpyHostToDevice);
+    if (err != cudaSuccess) {
+        printf("%s in %s at line %d\n", cudaGetErrorString(err), __FILE__, __LINE__);
+        exit(EXIT_FAILURE);
+    }
+
+    err = cudaMemcpy(B_d, B_h, size, cudaMemcpyHostToDevice);
+    if (err != cudaSuccess) {
+        printf("%s in %s at line %d\n", cudaGetErrorString(err), __FILE__, __LINE__);
+        exit(EXIT_FAILURE);
+    }
+
+    int threads = 256;
+    int blocks = (n + threads - 1) / threads;
+    vecAddKernel<<<blocks, threads>>>(A_d, B_d, C_d, n);
+
+    cudaMemcpy(C_h, C_d, size, cudaMemcpyDeviceToHost);
+
+    err = cudaFree(A_d);
+    if (err != cudaSuccess) {
+        printf("%s in %s at line %d\n", cudaGetErrorString(err), __FILE__, __LINE__);
+        exit(EXIT_FAILURE);
+    }
+
+    err = cudaFree(B_d);
+    if (err != cudaSuccess) {
+        printf("%s in %s at line %d\n", cudaGetErrorString(err), __FILE__, __LINE__);
+        exit(EXIT_FAILURE);
+    }
+
+    err = cudaFree(C_d);
+    if (err != cudaSuccess) {
+        printf("%s in %s at line %d\n", cudaGetErrorString(err), __FILE__, __LINE__);
+        exit(EXIT_FAILURE);
+    }
+}
+
+int main() {
+    const int size = 50000000;
+    float *A_h = (float *)malloc(size * sizeof(float));
+    float *B_h = (float *)malloc(size * sizeof(float));
+    float *C_h = (float *)malloc(size * sizeof(float));
+
+    if (!A_h || !B_h || !C_h) {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+
+    for (int i = 0; i < size; i++) {
+        A_h[i] = i;
+        B_h[i] = 49999999.0 - i;
+    }
+
+    vecAdd(A_h, B_h, C_h, size);
+
+    printf("Done\n");
+
+    return 0;
+}
